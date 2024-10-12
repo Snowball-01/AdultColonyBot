ec0976cfda85bdf9134f8ae79b1f3d0092d623b8
Snowball-01 <riteshraushan60@gmail.com>
2024-10-12 06:49:26 +0530

    fixed erros

--- plugins/AAForce_Sub.py
+++ plugins/AAForce_Sub.py
@@ -22,13 +22,24 @@ async def not_subscribed(_, client, message):
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
-    buttons = [[InlineKeyboardButton(
-        text="✿ Jᴏɪɴ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ✿", url=f"https://t.me/{Config.FORCE_SUB}") ]]
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="✿ Jᴏɪɴ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ✿", url=f"https://t.me/{Config.FORCE_SUB}"
+            )
+        ]
+    ]
     text = "**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ ☹️. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴄᴏɴᴛɪɴᴜᴇ**"
     try:
         user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)
         if user.status == enums.ChatMemberStatus.BANNED:
-            return await client.send_message(message.from_user.id, text="Sᴏʀʀy Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Tᴏ Uꜱᴇ Mᴇ")
+            return await client.send_message(
+                message.from_user.id, text="Sᴏʀʀy Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Tᴏ Uꜱᴇ Mᴇ"
+            )
     except UserNotParticipant:
-        return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
-    return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+        return await message.reply_text(
+            text=text, reply_markup=InlineKeyboardMarkup(buttons)
+        )
+    return await message.reply_text(
+        text=text, reply_markup=InlineKeyboardMarkup(buttons)
+    )
